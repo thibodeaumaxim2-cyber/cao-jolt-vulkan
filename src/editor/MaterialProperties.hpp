b3c51f535dd4f6df1ee60833c458bf5cdbb900c4
thibodeaumaxim2-cyber <thibodeaumaxim2@gmail.com>
2026-08-29 15:15:48 -0400

    Add CAO material properties

---
+++ src/editor/MaterialProperties.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+struct MaterialProperties {
+  float density = 7850.0f;
+  float youngsModulus = 200.0e9f;
+  float compressiveStrength = 250.0e6f;
+  float tensileStrength = 250.0e6f;
+};
