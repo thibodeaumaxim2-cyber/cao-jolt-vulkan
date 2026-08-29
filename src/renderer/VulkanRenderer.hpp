ed60221b78ba990a723ce897454b3e9efcbd45fd
thibodeaumaxim2-cyber <thibodeaumaxim2@gmail.com>
2026-08-29 13:51:51 -0400

    Use shared vertex format in Vulkan renderer

--- src/renderer/VulkanRenderer.hpp
+++ src/renderer/VulkanRenderer.hpp
@@ -1,22 +1,35 @@
 #pragma once
+
 #include "editor/Scene.hpp"
 #include "editor/Camera.hpp"
+#include "Vertex.hpp"
 #include <vulkan/vulkan.h>
 #include <vector>
 
-struct CadVertex { float position[3]; float color[3]; };
 class VulkanRenderer {
  public:
-  // The application supplies its swapchain/device handles after initialization.
-  void initialize(VkDevice,VkPhysicalDevice,VkRenderPass,VkExtent2D,VkFormat);
-  void setScene(const Scene*,const OrbitCamera*,uint32_t selected);
-  void record(VkCommandBuffer,VkFramebuffer);
+  void initialize(VkDevice, VkPhysicalDevice, VkRenderPass, VkExtent2D, VkFormat);
+  void setScene(const Scene*, const OrbitCamera*, uint32_t selected);
+  void record(VkCommandBuffer, VkFramebuffer);
   void resize(VkExtent2D);
   void shutdown();
+
  private:
-  VkDevice device_=VK_NULL_HANDLE; VkPipeline pipeline_=VK_NULL_HANDLE; VkPipelineLayout layout_=VK_NULL_HANDLE;
-  VkBuffer vertexBuffer_=VK_NULL_HANDLE,indexBuffer_=VK_NULL_HANDLE; VkDeviceMemory vertexMemory_=VK_NULL_HANDLE,indexMemory_=VK_NULL_HANDLE;
-  VkExtent2D extent_{}; const Scene* scene_=nullptr; const OrbitCamera* camera_=nullptr; uint32_t selected_=0;
-  std::vector<CadVertex> vertices_; std::vector<uint32_t> indices_;
-  void makeCubeMesh(); void uploadMesh(); void makePipeline(VkRenderPass,VkFormat);
+  VkDevice device_ = VK_NULL_HANDLE;
+  VkPipeline pipeline_ = VK_NULL_HANDLE;
+  VkPipelineLayout layout_ = VK_NULL_HANDLE;
+  VkBuffer vertexBuffer_ = VK_NULL_HANDLE;
+  VkBuffer indexBuffer_ = VK_NULL_HANDLE;
+  VkDeviceMemory vertexMemory_ = VK_NULL_HANDLE;
+  VkDeviceMemory indexMemory_ = VK_NULL_HANDLE;
+  VkExtent2D extent_{};
+  const Scene* scene_ = nullptr;
+  const OrbitCamera* camera_ = nullptr;
+  uint32_t selected_ = 0;
+  std::vector<CadVertex> vertices_;
+  std::vector<uint32_t> indices_;
+
+  void makeCubeMesh();
+  void uploadMesh();
+  void makePipeline(VkRenderPass, VkFormat);
 };
